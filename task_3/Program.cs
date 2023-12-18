a5e1eb7225c498d221b37dea6d255c05b66f6079
Alexander <sasha-naumov-2000@mail.ru>
2023-12-19 01:29:00 +0300

    3_task_done

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[5] { 1, 2, 3, 4, 5 };
+int m = 1;
+void ReversePrint(int[] array, int m)
+{
+
+    System.Console.WriteLine(array[(array.Length) - m]);
+    if (m != array.Length)
+    {
+        ReversePrint(array, m + 1);
+    }
+}
+ReversePrint(array, m);

50f33b948cfff078a7fffe102f25f7b55a3aa3a1
Alexander <sasha-naumov-2000@mail.ru>
2023-12-19 00:19:45 +0300

    truely working task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿int m = 5;
+int n = -142;
+
+void PrintNumbers(int m, int n)
+{  int i=m;
+if(i>0){
+    System.Console.Write(i+" ");}
+if (i!=n){
+    if(n>m)
+    {
+        PrintNumbers(m+1,n);
+    }
+     if(n<m)
+    {
+        PrintNumbers(m-1, n);
+    }
+}
+
+}
+PrintNumbers(m,n);
+// i=2
+// выводим 2
+// если 2!=11
+// то 
+// если 11>2
+// то 
+// выводим 
